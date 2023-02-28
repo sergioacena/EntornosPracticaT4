ec7d19fa413d4d0b260eba3ab0335aae7f5432dc
JorgeDLMV <jormoreva@gmail.com>
2023-02-28 20:36:57 +0100

    Formateado código y comentador

--- EntornosPracticaT4/src/paquete1/Personaje.java
+++ EntornosPracticaT4/src/paquete1/Personaje.java
@@ -5,18 +5,14 @@ public class Personaje {
 	private int vida;
 	private int danio;
 	
-	
 	public Personaje(int vida, int danio) {
 		this.vida = vida;
 		this.danio = danio;
 	}
 	
-	
-	
 	public int getDanio() {
 		return danio;
 	}
-
 
 	/**
 	 * METODO DE QUITAR VIDA
@@ -25,8 +21,5 @@ public class Personaje {
 	public void quitarVida(int danio) {
 		vida = vida - danio;
 	}
-	
-	
-	
 	
 }
