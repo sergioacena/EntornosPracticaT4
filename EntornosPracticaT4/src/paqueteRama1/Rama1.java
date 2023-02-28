b9893a759b86e9dc220c63e8acbbbe54eb885c6d
Sergio <Sergio@DESKTOP-3EOQJ7K>
2023-02-28 20:54:09 +0100

    PRIMER CAMBIO RAMA 1

--- EntornosPracticaT4/src/paqueteRama1/Rama1.java
+++ EntornosPracticaT4/src/paqueteRama1/Rama1.java
@@ -3,7 +3,9 @@ package paqueteRama1;
 public class Rama1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola desde la rama 1");
+		
 	}
 
 }
